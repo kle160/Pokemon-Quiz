db00d25c61ceecac91404ee92fc518a5121819d1
kle160 <lekevin2013@gmail.com>
2022-05-22 19:32:02 -0700

    Initial setup for App

---
+++ client/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './components/Main.jsx';
+
+function App() {
+  return(
+    <div className='App'>
+        <h1> Hello Asshole </h1>
+        <Main/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className='contacts'>
+            <h1>HELLO DUDE!</h1>
+        </div>
+    )
+}
+
+export default Main;
