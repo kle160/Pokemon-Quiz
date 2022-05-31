ef8d3e31db69872308a3ef3915c62c05d6aa77d6
kle160 <lekevin2013@gmail.com>
2022-05-30 21:36:19 -0700

    Updated Typeeffect

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import './main.css';
 import title from '../Assets/pokemonLogo.png';
+import Typewriter from 'typewriter-effect';
 
 const Main = () => {
+
     return (
         <div className="container-fluid bg-container">
             <h1><img src={title} alt="title logo" width="300" height="100"/></h1>
-            <h2>Welcome to the Pokemon Quiz! <br/> Please Enter Your Name Below to Get Started</h2>
-            <br/>
+            <h2>
+            <Typewriter
+                onInit={(typewriter) => {
+                    typewriter.typeString("Welcome to the Pokemon Quiz!")
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString("Please Enter Your Name Below to Get Started!")
+                    .start();
+                }}
+            />
+            </h2>
             <form className='inputForm'>
-                <input type="text" />
+                <input type="text" required/>
                 <span> </span>
                 <button onClick={''}>Enter</button>
             </form>
